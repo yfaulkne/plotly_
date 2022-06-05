a2d94c322dab5c8ab1139d246ed51b26a422ea2a
Yakeemah Faulkner <yfaulkne@gmail.com>
2022-06-05 14:27:31 -0400

    updated html

--- js/charts.js
+++ js/charts.js
@@ -59,7 +59,7 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("JS/data/samples.json").then((data) => {
+  d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
